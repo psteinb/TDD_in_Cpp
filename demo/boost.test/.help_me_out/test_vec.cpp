db5e1474b424561f49a6aeedf49cbbdab5d20399
Peter Steinbach <steinbach@scionics.de>
2014-11-13 15:30:23 +0100

    adding demo code

---
+++ demo/boost.test/.help_me_out/test_vec.cpp
@@ -0,0 +1,36 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE geo_vec
+#include <boost/test/unit_test.hpp>
+#include <vector>
+
+// BOOST_AUTO_TEST_CASE( geo_vec_callable )
+// {
+//   BOOST_CHECK_MESSAGE(true, "placeholder");
+// }
+
+
+// struct vec_fixture
+// {
+  
+//   std::vector<int> numbers;
+  
+//   vec_fixture():
+//     numbers(42)
+//   { 
+//     std::fill(numbers.begin(),numbers.end(),1 );
+//     BOOST_MESSAGE( "setup fixture" );
+//   };
+
+//   virtual ~vec_fixture()  { BOOST_MESSAGE( "teardown fixture" ); };
+
+
+// };
+
+// BOOST_FIXTURE_TEST_SUITE( geo_vec_norm, vec_fixture )
+ 
+// BOOST_AUTO_TEST_CASE( norm_is_sum )
+// {
+//   BOOST_CHECK_MESSAGE(true, "default queue does not show size 0! ");
+// }
+
+// BOOST_AUTO_TEST_SUITE_END()
---
+++ demo/boost.test/test_vec.cpp
@@ -0,0 +1,9 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE geo_vec
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE( starting_point )
+{
+  BOOST_FAIL("Nothing to test yet");
+}
+
